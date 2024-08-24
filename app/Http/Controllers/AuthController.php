0152a370894dc5cb3a7383097ca38aee45371c43
C1084-a-kai <C1084@oic.jp>
2024-08-24 22:22:27 +0900

    SignInを作成。

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignupRequest;
+use App\Http\Requests\SigninRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -18,5 +19,19 @@ class AuthController extends Controller
         $token = $user->createToken('token')->plainTextToken;
 
         return response()->json(['token' => $token], 201);
+    }
+
+    public function signin(SigninRequest $request)
+    {
+        $user = User::firstWhere('user_id', $request->user_id);
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['error_message' => 'Unauthorized'], 401);
+        }
+
+        $user->tokens()->delete();
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
     }
 }
