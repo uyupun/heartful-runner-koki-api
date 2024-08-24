7e4ae996e221ca792160b04b226404b0a0927d0e
C1084-a-kai <C1084@oic.jp>
2024-08-24 22:29:18 +0900

    認証できているかテストする用。

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 
 Route::prefix('auth')->group(function () {
     Route::post('/signup', [AuthController::class, 'signup']);
     Route::post('/signin', [AuthController::class, 'signin']);
 });
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('/hello', function () {
     return response()->json(['message' => 'hello, world!']);
