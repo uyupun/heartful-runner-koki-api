8d3ab15f7fdb542100c07ea05d4d6f1028c0403a
C1084-a-kai <C1084@oic.jp>
2024-08-24 22:15:38 +0900

    SignUpRequestを作成。

---
+++ app/Http/Requests/SignUpRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class SignUpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|string|max:16|unique:users,user_id',
+            'password' => 'required|string|min:8|max:255',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json(
+            [
+                'error_message' => $validator->errors(),
+            ],
+            400
+        );
+        throw new HttpResponseException($res);
+    }
+}
