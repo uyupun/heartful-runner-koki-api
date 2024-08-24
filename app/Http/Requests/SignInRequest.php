1c2e3144a6db4b0505d0978dc464f73da16c854b
C1084-a-kai <C1084@oic.jp>
2024-08-24 22:22:01 +0900

    SignInRequestを作成。

---
+++ app/Http/Requests/SignInRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class SignInRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json(
+            [
+                'error_message' => $validator->errors(),
+            ],
+            400
+        );
+        throw new HttpResponseException($res);
+    }
+}
