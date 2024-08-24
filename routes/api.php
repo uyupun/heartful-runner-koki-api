63dd522df5448e7425428aed7d9e0fdd201a7e12
C1084-a-kai <C1084@oic.jp>
2024-08-24 22:17:22 +0900

    SignUpへのrouteを作成。

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/signup', [AuthController::class, 'signup']);
+});
 
 Route::get('/hello', function () {
     return response()->json(['message' => 'hello, world!']);
