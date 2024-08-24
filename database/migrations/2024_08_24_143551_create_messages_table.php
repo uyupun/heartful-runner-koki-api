fca53e6fa2e26ce5d8790c95cf3b4503b34dd782
C1084-a-kai <C1084@oic.jp>
2024-08-25 00:15:25 +0900

    カラム名をnameからwriter_nameに変更。

--- database/migrations/2024_08_24_143551_create_messages_table.php
+++ database/migrations/2024_08_24_143551_create_messages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('content');
             $table->foreignId('writer_id')->constrained('users');
-            $table->string('name');
+            $table->string('writer_name');
             $table->timestamps();
         });
     }
