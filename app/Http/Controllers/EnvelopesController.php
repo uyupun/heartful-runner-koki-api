6f6ba8a1e841bf87df89271fd51ee3b2d196a2df
C1084-a-kai <C1084@oic.jp>
2024-08-25 02:28:45 +0900

    envelopes_api

---
+++ app/Http/Controllers/EnvelopesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Envelope;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class EnvelopesController extends Controller
+{
+    public function create(Request $request)
+    {
+        $envelope = Envelope::create([
+            'code' => Str::uuid()->toString(),
+            'title' => $request->title,
+            'sender_id' => $request->user()->id,
+            'holder_id' => $request->user()->id,
+            'state' => 'IN_PROGRESS',
+        ]);
+
+        return response()->json($envelope, 201);
+    }
+}
